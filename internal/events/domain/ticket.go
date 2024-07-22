5cbcabcd5315d8b8f8d4a5e86f33fb1df503315b
Rafaela Lima <ral@icomp.ufam.edu.br>
2024-07-22 18:31:54 -0400

    feat: add partners infra

--- internal/events/domain/ticket.go
+++ internal/events/domain/ticket.go
@@ -1,5 +1,7 @@
 package domain
 
+import "errors"
+
 type Ticket struct {
 	ID         string
 	EventID    string
@@ -14,3 +16,22 @@ const (
 	TicketTypeHalf TicketType = "HALF"
 	TicketTypeFull TicketType = "FULL"
 )
+
+var ErrInvalidTicketPriceZero = errors.New("ticket price must be greater than 0")
+
+func IsValidTicketType(ticketType TicketType) bool {
+	return ticketType == TicketTypeHalf || ticketType == TicketTypeFull
+}
+
+func (t *Ticket) CalculatePrice() {
+	if t.TicketType == TicketTypeHalf {
+		t.Price /= 2
+	}
+}
+
+func (t *Ticket) Validate() error {
+	if t.Price <= 0 {
+		return ErrInvalidTicketPriceZero
+	}
+	return nil
+}
